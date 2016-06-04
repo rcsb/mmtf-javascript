7302032021c907a0de38ab819e06b199fa3d65ca
Alexander Rose <alex.rose@rcsb.org>
2016-06-03 17:42:40 -0700

    updated data files for functional tests

--- test/tests-functional.js
+++ test/tests-functional.js
@@ -16,19 +16,19 @@ function loadFile( url, onload, onerror ){
 //
 QUnit.module( "functional tests" );
 
-// curl http://mmtf.rcsb.org/testdata/3zyb.mmtf.gz -o data/3zyb.mmtf.gz && gzip -df data/3zyb.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/4cup.mmtf.gz -o data/4cup.mmtf.gz && gzip -df data/4cup.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/1o2f.mmtf.gz -o data/1o2f.mmtf.gz && gzip -df data/1o2f.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/1bna.mmtf.gz -o data/1bna.mmtf.gz && gzip -df data/1bna.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/4y60.mmtf.gz -o data/4y60.mmtf.gz && gzip -df data/4y60.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/1skm.mmtf.gz -o data/1skm.mmtf.gz && gzip -df data/1skm.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/1lpv.mmtf.gz -o data/1lpv.mmtf.gz && gzip -df data/1lpv.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/1msh.mmtf.gz -o data/1msh.mmtf.gz && gzip -df data/1msh.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/1r9v.mmtf.gz -o data/1r9v.mmtf.gz && gzip -df data/1r9v.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/5emg.mmtf.gz -o data/5emg.mmtf.gz && gzip -df data/5emg.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/4ck4.mmtf.gz -o data/4ck4.mmtf.gz && gzip -df data/4ck4.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/4v5a.mmtf.gz -o data/4v5a.mmtf.gz && gzip -df data/4v5a.mmtf.gz;
-// curl http://mmtf.rcsb.org/testdata/5esw.mmtf.gz -o data/5esw.mmtf.gz && gzip -df data/5esw.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/3zyb.mmtf.gz -o data/3zyb.mmtf.gz && gzip -df data/3zyb.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/4cup.mmtf.gz -o data/4cup.mmtf.gz && gzip -df data/4cup.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/1o2f.mmtf.gz -o data/1o2f.mmtf.gz && gzip -df data/1o2f.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/1bna.mmtf.gz -o data/1bna.mmtf.gz && gzip -df data/1bna.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/4y60.mmtf.gz -o data/4y60.mmtf.gz && gzip -df data/4y60.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/1skm.mmtf.gz -o data/1skm.mmtf.gz && gzip -df data/1skm.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/1lpv.mmtf.gz -o data/1lpv.mmtf.gz && gzip -df data/1lpv.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/1msh.mmtf.gz -o data/1msh.mmtf.gz && gzip -df data/1msh.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/1r9v.mmtf.gz -o data/1r9v.mmtf.gz && gzip -df data/1r9v.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/5emg.mmtf.gz -o data/5emg.mmtf.gz && gzip -df data/5emg.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/4ck4.mmtf.gz -o data/4ck4.mmtf.gz && gzip -df data/4ck4.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/4v5a.mmtf.gz -o data/4v5a.mmtf.gz && gzip -df data/4v5a.mmtf.gz;
+// curl http://mmtf.rcsb.org/v0.2/full/5esw.mmtf.gz -o data/5esw.mmtf.gz && gzip -df data/5esw.mmtf.gz;
 
 var testEntries = [
     "3zyb", "4cup", "1o2f", "1bna", "4y60", "1skm", "1lpv", "1msh", "1r9v",
